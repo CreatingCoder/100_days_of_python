f12df76d14b8a3e96a3a4f43c7ec3aaee496d9b9
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-06-28 18:31:58 -0500

    Create band_name_gen.py

---
+++ day1/band_name_gen.py
@@ -0,0 +1,8 @@
+def bandNameGen():
+    print('Greetings!')
+    city = input('What city did you grow up in?')
+    pet = input('What was the name of your first pet?')
+    
+    return print('Your band name is ' + city + ' ' + pet)
+
+bandNameGen()
