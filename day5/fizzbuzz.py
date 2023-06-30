23cc8eeafe4027781380e4b7e03ca54eb3df1de2
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-06-30 23:19:18 +0000

    Update fizzbuzz.py

--- day5/fizzbuzz.py
+++ day5/fizzbuzz.py
@@ -1,12 +1,12 @@
 def fizzbuzz():
 
     for num in range(1,101,1):
-        
-        if((num % 3) == 0):
+        if((num % 3) == 0 and (num % 5 ) == 0):
+            print('fizzbuzz')
+        elif((num % 3) == 0):
             print('fizz')
         elif((num % 5 ) == 0):
             print('buzz')
         else:
             print(num)
-
 fizzbuzz()
