bed0c81c53835f727cae032e4bceeadaba6403eb
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-04 19:01:50 +0000

    Create greet_with.py

---
+++ day8/greet_with.py
@@ -0,0 +1,9 @@
+def greet_with(name, location):
+    print(f'Hello {name} !')
+    print(f'What is it like in {location} ?')
+
+
+#keyword arguments
+name = 'Shane'
+location = 'Maryland'
+greet_with(name, location)
