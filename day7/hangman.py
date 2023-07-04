a69ec475d886f406398b54b41dc71d72c64b8720
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-04 17:59:41 +0000

    Update hangman.py

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,22 +1,62 @@
 def hangman():
+    num_guess = 6
+    word_to_guess = 'ardvark'
+    list_of_guessed_letters = []
 
-    word = 'ardvark'
-    miss_count = 1
-    userInput = input('Please guess a letter:')
+    while num_guess > 0:
 
-    for j in range(0,6):
-        userInput = input('Please guess a letter:')
+        guessed_letter = input('Please guess a letter: ')
+        list_of_guessed_letters.append(guessed_letter)
 
-        for i in word:
+        if(guessed_letter not in word_to_guess):
+            num_guess = num_guess - 1
+        
+        output = hangman_output(word_to_guess,  list_of_guessed_letters)
 
-            if(i == userInput):
-                print(i,end = "")
-            else:
-                print('_', end ="")
-                      
-        print(f'You now have used {miss_count} of your 6 tries')
-        miss_count = miss_count + 1
+        if('_' not in output):
+            print('You won!')
+            break
 
+        if(num_guess == 6):
+            hangman_none()
+
+        elif(num_guess == 5):
+            hangman_one()
+
+        elif(num_guess == 4):
+            hangman_two()
+
+        elif(num_guess == 3):
+            hangman_three()
+
+        elif(num_guess == 2):
+            hangman_four()
+
+        elif(num_guess == 1):
+            hangman_five()
+       
+        elif(num_guess == 0):
+           hangman_six()
+           print(f'You lost! The word was {word_to_guess}')
+           break
+
+        print(output)
+    
+
+
+
+def hangman_output(w2g, logl):
+    output = ''
+
+    for i in w2g:
+
+        if(i in logl):
+            output = output + i
+        else:
+            output = output + '_'
+
+
+    return output
 
 def hangman_none():
     print('\n _______')
@@ -85,7 +125,7 @@ def hangman_five():
     print('|')
     print('____________')
 
-def hangman_five():
+def hangman_six():
     print('\n _______')
     print('|       |')
     print('|       O')
@@ -97,3 +137,7 @@ def hangman_five():
     print('____________')
 
 hangman()
+
+
+
+
