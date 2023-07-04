b606b58c5930b27830517043defc52701ecf1a2b
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-04 19:15:43 +0000

    Update paint_area_calc.py

--- day8/paint_area_calc.py
+++ day8/paint_area_calc.py
@@ -1,5 +1,7 @@
+import math
+
 def paint_area_calc(height, width, coverage):
     # number of cans = (wall height x wall width) / coverage per can.
-    return print(round((height * width) / coverage))
+    return print(int(math.ceil((height * width) / coverage)))
 
-paint_area_calc(height = 2, width=4, coverage=5)
+paint_area_calc(height = 7, width=13, coverage=5)
