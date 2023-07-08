a1284b8b648c90661eba1cdfb66a7b923ef6a001
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-08 06:52:39 -0500

    Update blackjack.py

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -3,7 +3,6 @@ import random
 def blackjack():
     play = True
 
-
     while play == True:
         user_card= [None] * 21
         cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
@@ -19,10 +18,9 @@ def blackjack():
         show_comp1 = random.choice(cards)
         hide_comp2 = random.choice(cards)
         
-        print(f'Your cards are: [{user_card[1]}, {user_card[2]}]')
-        print(f"The computer's first card is {show_comp1}")
+        final_cards = run(num_cards, user_card, show_comp1)
 
-        #run(,num_cards)
+        print(f"the player's final cards are: {final_cards}")
 
             
 
@@ -32,12 +30,31 @@ def total(a,b):
     """returns total of two variables"""
     return a+b
 
-def run(card_count):
+def run(card_count, players_cards, comp_shown_card):
+
+
+    print(f'Your cards are: {players_cards[:card_count]}')
+    print(f"The computer's first card is {comp_shown_card}")
+
     hit_me = input("Type 'y' to get another card, or 'n' to pass: ").lower()
+    
+
     if(hit_me == 'y'):
-        run()
-   
-    return print('yeet')
+        #add another card
+        card_count = card_count + 1 
+        run(card_count, players_cards, comp_shown_card)
+    
+    elif(hit_me == 'n'):
+        return players_cards[:card_count]
+    
+    #return print('yeet')
+
+
+        
+
+    
+
+    
     
 
     
