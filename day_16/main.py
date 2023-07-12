5908cb53e799248afb0fb156a34b210ef484522e
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-12 18:51:33 -0400

    Add files via upload

---
+++ day_16/main.py
@@ -0,0 +1,35 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+run = True
+
+while run == True:
+    cm = CoffeeMaker()
+    menu = Menu()
+    menu_item = MenuItem()
+    mm = MoneyMachine()
+
+    userinput = input(f'What would you like?  {menu.get_items()}: ').lower()
+
+    if(userinput == 'off'):
+        exit()
+
+    if(userinput == 'report'):
+        cm.report()
+
+    drink_order = menu.find_drink(userinput)
+
+    if cm.is_resource_sufficient(drink_order) == True:
+        print(menu_item.cost)
+        
+        #get cost of drink
+        
+
+        #mm.make_payment()
+
+
+        
+
+    
