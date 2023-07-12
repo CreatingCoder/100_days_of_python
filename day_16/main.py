9fd5229467b9f27657be3d16039032dfda189baa
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-12 18:11:14 -0500

    Update main.py

--- day_16/main.py
+++ day_16/main.py
@@ -8,10 +8,12 @@ run = True
 while run == True:
     cm = CoffeeMaker()
     menu = Menu()
-    menu_item = MenuItem()
+    
     mm = MoneyMachine()
 
     userinput = input(f'What would you like?  {menu.get_items()}: ').lower()
+
+    
 
     if(userinput == 'off'):
         exit()
@@ -20,10 +22,14 @@ while run == True:
         cm.report()
 
     drink_order = menu.find_drink(userinput)
+    
+
+    
+
 
     if cm.is_resource_sufficient(drink_order) == True:
-        print(menu_item.cost)
-        
+        #print(menu_item.cost)
+        print
         #get cost of drink
         
 
