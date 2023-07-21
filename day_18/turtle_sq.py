5b0e4761aa061ee5728df2433b2c67cadf74b91b
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-21 06:39:49 -0500

    Create turtle_sq.py

---
+++ day_18/turtle_sq.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+#create new turtle
+t = Turtle()
+
+#shape and color
+t.shape('turtle')
+t.color('green')
+
+#set movement by 4 times
+for i in range(4):
+    for i in range(10):
+        t.forward(25)
+    t.right(90)
+
+
+#create screen 
+scr = Screen()
+scr.exitonclick()
+
+
+
