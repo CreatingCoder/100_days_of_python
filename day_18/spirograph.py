8898eb881bb02f5f22dd5b2195a0ca4e6c474a40
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-22 07:03:48 -0500

    Create spirograph.py

---
+++ day_18/spirograph.py
@@ -0,0 +1,53 @@
+from turtle import Turtle, Screen
+from random import Random
+
+color_list = ['red', 'orange', 'yellow', 'green', 'blue',
+  'purple', 'pink', 'brown', 'gray', 'gold']
+
+dir = [0, 90, 180, 180+90]
+
+#create new turtle
+t = Turtle()
+
+#create random inst
+rand = Random()
+
+#shape and color
+#t.shape('circle')
+
+t.pensize(width = 5)
+t.shapesize(stretch_wid=.5)
+
+rand_color = rand.randint(0,9)
+t.color(color_list[rand_color])
+
+for i in range(18):
+    #rand_int = rand.randint(0,3)
+    t.forward(20)
+    t.right(20) #dir[rand_int]
+
+#moves circle starting point
+t.right(10)
+
+for i in range(18):
+  #rand_int = rand.randint(0,3)
+  t.forward(20)
+  t.right(20) #dir[rand_int]
+
+
+
+
+
+
+
+
+
+
+
+
+#create screen 
+scr = Screen()
+scr.exitonclick()
+
+
+
