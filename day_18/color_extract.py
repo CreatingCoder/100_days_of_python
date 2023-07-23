67191038ac96f28948e8e6ead2ba5bdc1b049ead
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-23 01:35:58 +0000

    Update color_extract.py

--- day_18/color_extract.py
+++ day_18/color_extract.py
@@ -8,7 +8,12 @@ colors = cg.extract('day_18/image.jpg', 20)
 rgb_list = []
 
 for i in colors:
-    rgb_list.append(i.rgb)
+    r = i.rgb.r
+    g = i.rgb.g
+    b = i.rgb.b
 
-print(rgb_list[0])
+    new_color = (r,g,b)
+    rgb_list.append(new_color)
+
+print(rgb_list)
     
