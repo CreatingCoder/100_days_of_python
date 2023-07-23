f3f08f0f1ef1a8fad313f10ae605a2b8faf97f63
Shane <creatingcoder@gmail.com~>
2023-07-23 00:33:09 +0000

    updated

--- day_18/color_extract.py
+++ day_18/color_extract.py
@@ -1,3 +1,16 @@
 import colorgram as cg
 
-colors = cg.extract('image.jpg', 20)
+#creates list of colors used
+colors = cg.extract('day_18/image.jpg', 20)
+
+#       RGB                        PROPORTION
+#Color: Rgb(r=234, g=234, b=234), 4.345
+
+rgb_list = []
+
+
+for i in colors:
+    rgb_list.append(i.rgb)
+
+print(rgb_list[0])
+    
