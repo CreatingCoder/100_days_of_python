73026a4ade865dd80ffb020ee8e48b32b5db744f
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-26 07:41:35 -0400

    Create snake.py

---
+++ day_20/snake.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+import random as rand
+
+scr = Screen()
+scr.setup(width=600, height=600)
+scr.title('My Snake Game')
+scr.bgcolor('black')
+
+t = Turtle()
+
+t.shape('square')
+t.color('white')
+
+
+
+
+
+
+      
+
+scr.exitonclick()
