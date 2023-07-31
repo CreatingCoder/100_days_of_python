68f3a29e9e3f05e486c313dd67d09c936255833b
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-07-31 18:20:36 -0500

    Update scoreboard.py

--- day_20/snake_oop/scoreboard.py
+++ day_20/snake_oop/scoreboard.py
@@ -1,6 +1,9 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('times new roman', 24, 'normal')
 
 class Scoreboard(Turtle):
+
     
     def __init__(self):
         super().__init__()
@@ -12,13 +15,17 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
         
     def update_scoreboard(self):
-        self.write(f"Score:{self.score}", align = 'center', font =( 'arial', 24, 'normal'))
+        self.write(f"Score:{self.score}", align = ALIGNMENT, font =FONT)
 
 
     def increase_score(self):
         self.score = self.score + 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write('GAME OVER', align=ALIGNMENT, font = FONT)
     
     
 
