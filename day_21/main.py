6fe516bb31936e60eb6585e2e1beb7de34a42f07
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-01 20:55:17 -0400

    Create main.py

---
+++ day_21/main.py
@@ -0,0 +1,35 @@
+from turtle import Turtle, Screen
+import random as rand
+from pong import Pong
+
+scr = Screen()
+scr.bgcolor('black')
+scr.setup(800,600)
+
+pong = Pong()
+
+pong.movement()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+scr.exitonclick()
+
