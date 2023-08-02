7dabf4e132af908cb69cbbb03401a67de21411cd
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-01 21:14:59 -0400

    Update main.py

--- day_21/main.py
+++ day_21/main.py
@@ -1,17 +1,22 @@
 from turtle import Turtle, Screen
 import random as rand
 from pong import Pong
+import time
 
 scr = Screen()
 scr.bgcolor('black')
 scr.setup(800,600)
+#scr.tracer(0)
+scr.listen()
 
 pong = Pong()
 
 pong.movement()
 
-
-
+run = True
+#while run == True:
+    #time.sleep(0.1)
+    #scr.update()
 
 
 
