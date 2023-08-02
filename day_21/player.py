155d8312ffcfdce9bd064339d6d0b57e6c9b6d91
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-02 15:19:12 -0500

    Create player.py

---
+++ day_21/player.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+import random as rand
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.color('white')
+        self.hideturtle()
+        self.penup()
+        self.setposition(300, 0)
+        self.showturtle()
