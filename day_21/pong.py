8525dd61e2c62d0ed5d7bff12bd43fae4a2a82fa
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-02 20:37:15 -0400

    Update pong.py

--- day_21/pong.py
+++ day_21/pong.py
@@ -6,33 +6,27 @@ class Pong(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.hideturtle()
         self.shape('square')
         self.color('white')
         self.penup()
         self.speed('slowest')
+        random_y = rand.randint(-270,270)
+        self.sety(random_y)
+        self.setheading(rand.randint(0, 360))
         
        
-
-
     def movement(self):
-        #random number determines x direction
-        random_x = rand.randint(0,1)
-
         #random number determines y location
-        random_y = rand.randint(-270,270)
+        
+        self.showturtle()
+        self.fd(self.heading())
 
-        self.hideturtle()
-        self.sety(random_y)
-
-
-        #left
-        if random_x == 0:
-            self.left(180)
-            self.showturtle()
-            self.fd(90)
-
-        #right
-        elif random_x == 1:
-            self.right(0)
-            self.showturtle()
-            self.fd(90)
+    def bounds(self):
+        if self.ycor() >= 270 or self.ycor() <= -270:
+            #self.setheading(self.heading() + 180)
+            self.backward(self.heading() + 600)
+        if self.xcor() >=380 or self.xcor() <= -380:
+            exit()
+        
+        
