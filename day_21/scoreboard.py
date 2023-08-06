ac110e6f1595371ebc72eeb6f5333f3c9a7b3b98
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-06 18:40:34 -0500

    Update scoreboard.py

--- day_21/scoreboard.py
+++ day_21/scoreboard.py
@@ -5,4 +5,31 @@ class Scoreboard(Turtle):
 
 
     def __init__(self):
-        pass
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.color('white')
+        self.left_score = 0
+        self.right_score = 0
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 200)
+        self.write(self.left_score, align = 'center', font = ('Courier',80, 'normal'))
+        self.goto(100, 200)
+        self.write(self.right_score, align = 'center', font = ('Courier',80, 'normal'))
+
+
+
+    def add_left(self):
+        self.left_score = self.left_score + 1
+        self.update_scoreboard()
+
+    def add_right(self):
+        self.right_score = self.right_score + 1
+        self.update_scoreboard()
+
+
+        
