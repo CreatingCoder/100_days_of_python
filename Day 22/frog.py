1d5f7b1c314caccc24335f320f4c4837a7d23044
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-07 19:51:30 +0000

    Add files via upload

---
+++ Day 22/frog.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+
+class Frog(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape('turtle')
+        self.setheading(90)
+        self.setpos(0, -320)
+        self.color('green')
+
+
+    def up(self):
+        #self.goto(self.xcor() + 90)
+        self.forward(20)
