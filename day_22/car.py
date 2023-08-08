39cf6cee9269e3e74ff447ee85115a4c4753e6be
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-07 21:36:13 -0400

    Update car.py

--- day_22/car.py
+++ day_22/car.py
@@ -1,4 +1,7 @@
 from turtle import Turtle, Screen
+import random as rand
+
+COLORS = COLORS = ['yellow', 'green', 'blue', 'red']
 
 
 class Car(Turtle):
@@ -6,3 +9,16 @@ class Car(Turtle):
     def __init__(self):
         super().__init__()
         self.shape('square')
+        #self.y_cor = 
+        self.setposition(300, rand.randint(-280, 280))
+        self.penup()
+        self.shapesize(1, 2)
+        self.color(COLORS[rand.randint(0, len(COLORS)-1)])
+        self.setheading(180)
+
+
+    def move_car(self):
+        self.forward(0.3)
+
+
+        
