4988f6df3a67385fac570154b2e2050046fe9bde
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-11 13:51:55 -0400

    Update main.py

--- day_22/main.py
+++ day_22/main.py
@@ -2,6 +2,8 @@ from turtle import Turtle, Screen
 import random as rand
 from frog import Frog
 from car import Car
+from numpy.random import permutation
+
 
 
 scr = Screen()
@@ -14,16 +16,46 @@ scr.listen()
 frog = Frog()
 
 
+
+
+
 scr.onkeypress(frog.up, 'Up')
 
+list = []
+for i in range(10):
+        list.append(Car())
+        
 
 run = True
 while run == True:
+    x = 1
     scr.update()
 
-    list = []
-    for i in range(10):
-        list.append(Car())
+    
+    #if x % 3 == 1.5:
+          #list.append(Car())
+    
+          
+          
+    
+    for cars in list:
+          cars.move_car()
+          if cars.pos() == frog.pos():
+            
+            exit()
+
+
+    
+
+    x = x+1
+        
+
+    
+
+    
+    
+
+    
 
 
 
