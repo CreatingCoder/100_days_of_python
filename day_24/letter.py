24f09f33e1a79543a3dbb94a7cf3bd586638cbdb
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-08-25 16:13:14 -0500

    Update letter.py

--- day_24/letter.py
+++ day_24/letter.py
@@ -1,12 +1,21 @@
 from docx import Document
 
+username = ''
+address_1 = ''
+address_2 = ''
+address_3 = ''
 to_block = ''
 
+username = input('Please enter your name ')
+address_1  = input('Please enter your address line 1')
+address_2 = input('Please enter your address line 2')
+address_3 = input('Please enter your address line 3 (if applicable) ')
 to_block = input('Please enter the title/name the letter is being sent to: ')
 
 
-
 doc = Document()
+
+doc.add_paragraph(f'{username}\n{address_1}\n {address_2}\n {address_3}')
 
 doc.add_paragraph(f'Dear {to_block}')
 
