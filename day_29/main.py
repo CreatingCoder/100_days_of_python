f66269eed381b4fed878b347d1d48cb8d6093927
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-11-21 02:22:15 +0000

    Update main.py

--- day_29/main.py
+++ day_29/main.py
@@ -1,1 +1,31 @@
-print('hello world')
+from tkinter import *
+
+window = Tk()
+window.title('Password Manager')
+window.config(padx=20, pady=20)
+window.geometry("500x800")
+
+
+canvas = Canvas(width=300, height=300, bg='gray', highlightthickness= 0)
+
+
+#Image
+lock_img = PhotoImage(file="lock.gif")
+canvas.create_image(150,112, image=lock_img)
+canvas.grid(column=4, row=0, pady=40, padx = 15)
+
+#
+L1 = Label(window, text="User Name")
+L1.grid(column=3, row=2,)
+
+
+E1 = Entry(window, bd =5)
+E1.grid(column=4, row=2,)
+
+
+
+
+
+
+
+window.mainloop()
