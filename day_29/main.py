0dc4652a904b3a63db49942cf49eabef8e9500ce
Shane <69217993+CreatingCoder@users.noreply.github.com>
2023-11-22 01:57:00 +0000

    Update main.py

--- day_29/main.py
+++ day_29/main.py
@@ -1,20 +1,30 @@
 from tkinter import *
+import random as rand
 
-#################3
+alphabet = "!#$%&\()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+password_length = 12
+
+
+#################
 
 def gen_pass():
-    print('Hello World')
+    for i in range(0,password_length):
+        i = i +1
+        # global password_str
+        password_str = password_str + alphabet[rand.randint(0,92)]
+
 
 def add():
     print('Add')
-    global entry, entry2
+    global entry
+    global entry2
     entry = E1.get()
     entry2 = E2.get()
     
     file = open('yeet.txt', 'a')
-    file.write(entry + '  |  ' + entry2 + '/n')
+    file.write(entry + '  |  ' + entry2 + '\n')
     
-
     file.close()
 
 
@@ -55,8 +65,8 @@ E2.grid(column=4, row=3)
 L2 = Label(window, text="Password")
 L2.grid(column=3, row=4,)
 
-E2 = Entry(window, bd =5)
-E2.grid(column=4, row=4)
+E3 = Entry(window, bd =5)
+E3.grid(column=4, row=4)
 
 gen_pass = Button(text='Generate Password', command = gen_pass)
 gen_pass.grid(column=4,row=5)
